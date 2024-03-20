482a73809cb39502a9c6cd583964a5047cb049c1
davidemarchini <marchini73@gmail.com>
2024-03-20 20:03:01 +0100

    aggiunta colonna description dentro trains_table

---
+++ database/migrations/2024_03_20_185115_add_description_to_trains_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+
+        $table->string('description')->after('azienda');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+};
